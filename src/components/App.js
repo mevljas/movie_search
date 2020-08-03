dd767650f88c925e39b15d3f904ce0046b895eb6
mevlja's <sm9299@student.uni-lj.si>
2020-08-03 20:36:55 +0200

    Imported other componets to the App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from "./Header";
+import Movie from "./Movie";
+import Search from "./Search";
 
 function App() {
   return (
