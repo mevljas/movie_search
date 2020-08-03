92a0a5b9893eadeacc65010e88830f058f6af11a
mevlja's <sm9299@student.uni-lj.si>
2020-08-03 21:18:02 +0200

    Updates

---
+++ precache-manifest.9127f2d740b39831307b0a24fcee0281.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59f525e0ca602ec5699e1f72a3df74c6",
+    "url": "/movie_search/index.html"
+  },
+  {
+    "revision": "0db13ffa984ba5f09815",
+    "url": "/movie_search/static/css/main.467f174e.chunk.css"
+  },
+  {
+    "revision": "0b7479e86e3dcdd1ebe3",
+    "url": "/movie_search/static/js/2.1086ade2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_search/static/js/2.1086ade2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0db13ffa984ba5f09815",
+    "url": "/movie_search/static/js/main.845069e6.chunk.js"
+  },
+  {
+    "revision": "74cecc12114c79badb65",
+    "url": "/movie_search/static/js/runtime-main.28552451.js"
+  }
+]);
